3dada65a522151df6ddd18b2b6baad3faf3d1ff6
Daniel H <dnlheelan@gmail.com>
2018-04-10 15:04:39 +0100

    Add files via upload

---
+++ VolumeOfSphere.java
@@ -0,0 +1,10 @@
+public class VolumeOfSphere{
+	public static float volume (float h, float r){
+		float PI = 3.14f;
+
+		r = 10;
+		h = 20;
+		float calculation = PI * r*r * h/3;
+		return calculation; 
+	}
+}
